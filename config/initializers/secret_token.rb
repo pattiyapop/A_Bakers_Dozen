9a0c6611964a5f3d0545b9af0819583d3c8b4b11
Sierra Flynn <sierra.flynn@colorado.edu>
2013-11-13 13:42:19 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ABakersDozen::Application.config.secret_token = '0a619af1ce899bd8951f77272cf75c3757af28f5392b8673be003be037cd514e9c3bd69533a7a6b52118d93a4aa7cedb6e1e305863573c03754927db5c72ef16'
