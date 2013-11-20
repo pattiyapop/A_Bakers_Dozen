45677a7199c3249ced814c4947d94a4ac346f48b
Pattiya Mahapasuthanon <poppy_navi@hotmail.com>
2013-11-20 12:47:20 -0800

    Updated comment.rb

---
+++ app/models/comment.rb
@@ -0,0 +1,21 @@
+class Comment < ActiveRecord::Base
+#==sub-header
+#1 list item #1
+#2 list item #2
+   def initialize
+   
+   end
+
+   def owner
+      
+   end
+
+   def comment
+   end
+
+   def date
+   end
+
+   def rating
+   end
+end
