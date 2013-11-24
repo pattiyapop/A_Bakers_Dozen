41de129a51e24c7c2461d929370dc5909e4a496c
Pattiya Mahapasuthanon <poppy_navi@hotmail.com>
2013-11-23 21:51:10 -0800

    Updated comment.rb

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,21 +1,12 @@
+#=header
 class Comment < ActiveRecord::Base
 #==sub-header
 #1 list item #1
 #2 list item #2
    def initialize
-   
-   end
-
-   def owner
-      
-   end
-
-   def comment
-   end
-
-   def date
-   end
-
-   def rating
+     @owner = 'N/A'
+     @comment = 'None' 
+     @date '2013-11-23'
+     @rating = 0
    end
 end
