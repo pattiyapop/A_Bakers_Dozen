bdf09fe9d7cad0ec7d7ee3c178e5e59599ef0f89
Michael Brughelli <michael.brughelli@colorado.edu>
2013-11-26 14:03:17 -0800

    fixed seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,4 +7,8 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
 users = [{:username => 'MBrugh', :name => 'Michael Brughelli'}, {:username => 'SFlynn', :name => 'Sierra Flynn'}, {:username => 'PMahap', :name => 'Pattiya Mahapasuthanon'}, {:username => 'ASchwa', :name => 'Arnold Schwarzenegger' }, {:username => 'SStall', :name => 'Silvester Stallone'}, {:username => 'BWilli', :name => 'Bruce Willis'}, {:username => 'JHetfi', :name => 'James Hetfield'}, {:username => 'LUlric' , :name => 'Lars Ulrich'}, {:username => 'KHamme', :name => 'Kirk Hammet'},{:username => 'CBurto', :name => 'Cliff Burton'}]
+
+users.each do |user|
+  User.create!(user)
+end
 	
