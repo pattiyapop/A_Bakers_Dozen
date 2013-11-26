e34f29692ac6073565a6d5d9ef8530d1d88ac3d2
Michael Brughelli <michael.brughelli@colorado.edu>
2013-11-26 12:43:33 -0800

    fixed schema

--- db/schema.rb
+++ db/schema.rb
@@ -32,4 +32,12 @@ ActiveRecord::Schema.define(:version => 20131126203307) do
     t.datetime "updated_at"
   end
 
+  create_table "users", :force => true do |t|
+    t.string   "username"
+    t.string   "name"
+    t.string   "picture"
+    t.text     "interests"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 end
