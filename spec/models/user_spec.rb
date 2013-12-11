783040759af531f0669e4f86d93126a2e74623a4
Sierra Flynn <sierra.flynn@colorado.edu>
2013-12-10 22:05:18 -0700

    working on ch 11, added relationship model o users can follow one another and associated rspec tests are working

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -9,6 +9,12 @@ describe User do
 
   it { should respond_to(:username) }
   it { should respond_to(:name) }
+  it { should respond_to(:relationships) }
+  it { should respond_to(:followed_users) }
+  it { should respond_to(:following?) }
+  it { should respond_to(:follow!) }
+  it { should respond_to(:reverse_relationships) }
+  it { should respond_to(:followers) }
 
   it { should be_valid }
 
@@ -22,4 +28,32 @@ describe User do
     before { @user.name = " " }
     it { should_not be_valid }
   end
+
+  #following
+  describe "following" do
+    let(:other_user) { FactoryGirl.create(:user) }
+    before do
+      @user.save
+      @user.follow!(other_user)
+    end
+
+    it { should be_following(other_user) }
+    its(:followed_users) { should include(other_user) }
+
+    describe "and unfollowing" do
+      before { @user.unfollow!(other_user) }
+
+      it { should_not be_following(other_user) }
+      its(:followed_users) { should_not include(other_user) }
+    end
+
+    it { should be_following(other_user) }
+    its(:followed_users) { should include(other_user) }
+
+    describe "followed user" do
+      subject { other_user }
+      its(:followers) { should include(@user) }
+    end
+  end
+
 end
