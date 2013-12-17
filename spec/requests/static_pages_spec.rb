8c4bf622269f41e344a30db7a3566c700fd6c105
Pattiya Mahapasuthanon <poppy_navi@hotmail.com>
2013-12-17 09:05:56 -0800

    Add following/followers stats

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -58,19 +58,7 @@ describe "Static pages" do
         expect(page).to have_title("A Baker's Dozen")
       end
 
-      describe "follower/following counts" do
-        let(:other_user) { FactoryGirl.create(:user) }
-        before do
-          other_user.follow!(user)
-          visit users_path
-      end
-
-        it { should have_link("0 following", href: following_user_path(user)) }
-        it { should have_link("1 followers", href: followers_user_path(user)) }
-      end
-    end
-
-    describe "Log-in page" do
+     describe "Log-in page" do
     
       it "should have h1 'A Baker's Dozen'" do
         visit signin_path
@@ -108,4 +96,33 @@ describe "Static pages" do
         expect(page).to have_title("A Baker's Dozen")
       end
     end
+
+    describe "Follow Recipes" do
+      describe "for signed-in users" do
+      let(:user) { FactoryGirl.create(:user) }
+      before do
+        FactoryGirl.create(:micropost, user: user, content: "Lorem")
+        FactoryGirl.create(:micropost, user: user, content: "Ipsum")
+        sign_in user
+        visit root_path
+      end
+
+      it "should render the user's feed" do
+        user.feed.each do |item|
+          expect(page).to have_selector("li##{item.id}", text: item.content)
+        end
+      end
+
+      describe "follower/following counts" do
+        let(:other_user) { FactoryGirl.create(:user) }
+        before do
+          other_user.follow!(user)
+          visit root_path
+        end
+
+        it { should have_link("0 following", href: following_user_path(user)) }
+        it { should have_link("1 followers", href: followers_user_path(user)) }
+      end
+    end
+  end
 end
